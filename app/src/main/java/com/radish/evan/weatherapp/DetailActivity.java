cfe65a152dffc92bb6bc6a64360aad02cd785602
evanradish <evanradish@gmail.com>
2016-07-07 20:54:21 -0500

    push before adding db

--- app/src/main/java/com/radish/evan/weatherapp/DetailActivity.java
+++ app/src/main/java/com/radish/evan/weatherapp/DetailActivity.java
@@ -2,12 +2,17 @@ package com.radish.evan.weatherapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,25 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.detail, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detailfragment, menu);
+
+        // Retrieve the share menu item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+
+        // Attach an intent to this ShareActionProvider.  You can update this at any time,
+        // like when the user selects a new piece of data they might like to share.
+        if (mShareActionProvider != null ) {
+            DetailActivityFragment detailActivityFragment = new DetailActivityFragment();
+            mShareActionProvider.setShareIntent(detailActivityFragment.createShareForecastIntent());
+        } else {
+            Log.d(DetailActivity.class.getSimpleName(), "INTO share Provider--"+mShareActionProvider.toString());
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
         return true;
     }
 
--- app/src/main/java/com/radish/evan/weatherapp/DetailActivityFragment.java
+++ app/src/main/java/com/radish/evan/weatherapp/DetailActivityFragment.java
@@ -13,19 +13,40 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+
+    public static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    public String mForecastStr;
+
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Intent intent = getActivity().getIntent();
+
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        // The detail Activity called via intent.  Inspect the intent for forecast data.
+        Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forcastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
             ((TextView) rootView.findViewById(R.id.detail_text))
-                    .setText(forcastStr);
+                    .setText(mForecastStr);
         }
+
         return rootView;
     }
+
+
+
+    public Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
+    }
 }
+
